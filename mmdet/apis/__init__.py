2f37d37a86a01eaab25a81f39ed3e521fa0627b4
Plutoyxt <88093829+Plutoyxt@users.noreply.github.com>
2022-03-19 20:52:04 +0800

    Update __init__.py

--- mmdet/apis/__init__.py
+++ mmdet/apis/__init__.py
@@ -9,5 +9,5 @@ from .obb.huge_img_inference import (get_windows, inference_detector_huge_image,
 __all__ = [
     'get_root_logger', 'set_random_seed', 'train_detector', 'init_detector',
     'async_inference_detector', 'inference_detector', 'show_result_pyplot',
-    'multi_gpu_test', 'single_gpu_test'
+    'multi_gpu_test', 'single_gpu_test','get_windows','inference_detector_huge_image','merge_patch_results'
 ]
