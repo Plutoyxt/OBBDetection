3573e39c1760e443a9cd53fce0bd00df431e019f
Plutoyxt <88093829+Plutoyxt@users.noreply.github.com>
2022-08-31 13:34:36 +0800

    Update coco_detection.py

--- configs/_base_/datasets/coco_detection.py
+++ configs/_base_/datasets/coco_detection.py
@@ -1,5 +1,5 @@
 dataset_type = 'CocoDataset'
-data_root = '/content/coco_style/'
+data_root = '/content/gdrive/MyDrive/ssdd_coco/'
 img_norm_cfg = dict(
     mean=[123.675, 116.28, 103.53], std=[58.395, 57.12, 57.375], to_rgb=True)
 train_pipeline = [
@@ -27,22 +27,23 @@ test_pipeline = [
             dict(type='Collect', keys=['img']),
         ])
 ]
+/content/gdrive/MyDrive/ssdd_coco/train/train_image
 data = dict(
     samples_per_gpu=2,
     workers_per_gpu=2,
     train=dict(
         type=dataset_type,
-        ann_file=data_root + 'annotations/train.json',
-        img_prefix=data_root + 'images/train/',
+        ann_file=data_root + 'train/train.json',
+        img_prefix=data_root + 'train/train_image/',
         pipeline=train_pipeline),
     val=dict(
         type=dataset_type,
-        ann_file=data_root + 'annotations/test.json',
-        img_prefix=data_root + 'images/test/',
+        ann_file=data_root + 'val/val.json',
+        img_prefix=data_root + 'val/val_image/',
         pipeline=test_pipeline),
     test=dict(
         type=dataset_type,
-        ann_file=data_root + 'annotations/test.json',
-        img_prefix=data_root + 'images/test/',
+        ann_file=data_root + 'test/test.json',
+        img_prefix=data_root + 'test/test_image/',
         pipeline=test_pipeline))
 evaluation = dict(interval=1, metric='bbox')
