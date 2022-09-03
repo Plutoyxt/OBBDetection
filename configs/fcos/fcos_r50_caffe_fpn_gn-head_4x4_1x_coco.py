bb416163aa7ba19138fa34ab17af01ca16319d7b
Plutoyxt <88093829+Plutoyxt@users.noreply.github.com>
2022-09-03 21:34:56 +0800

    Update fcos_r50_caffe_fpn_gn-head_4x4_1x_coco.py

--- configs/fcos/fcos_r50_caffe_fpn_gn-head_4x4_1x_coco.py
+++ configs/fcos/fcos_r50_caffe_fpn_gn-head_4x4_1x_coco.py
@@ -92,7 +92,7 @@ data = dict(
     test=dict(pipeline=test_pipeline))
 # optimizer
 optimizer = dict(
-    lr=0.001, paramwise_cfg=dict(bias_lr_mult=2., bias_decay_mult=0.))
+    lr=0.002, paramwise_cfg=dict(bias_lr_mult=2., bias_decay_mult=0.))
 optimizer_config = dict(
     _delete_=True, grad_clip=dict(max_norm=35, norm_type=2))
 # learning policy
@@ -102,4 +102,4 @@ lr_config = dict(
     warmup_iters=500,
     warmup_ratio=1.0 / 3,
     step=[8, 11])
-total_epochs = 50
+total_epochs = 60
