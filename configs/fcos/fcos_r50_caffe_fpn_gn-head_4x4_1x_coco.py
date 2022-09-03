a538500a869cf3a210cc7c1e3b75c97e604aa43e
Plutoyxt <88093829+Plutoyxt@users.noreply.github.com>
2022-09-04 04:56:55 +0800

    Update fcos_r50_caffe_fpn_gn-head_4x4_1x_coco.py

--- configs/fcos/fcos_r50_caffe_fpn_gn-head_4x4_1x_coco.py
+++ configs/fcos/fcos_r50_caffe_fpn_gn-head_4x4_1x_coco.py
@@ -86,7 +86,7 @@ test_pipeline = [
 ]
 data = dict(
     samples_per_gpu=8,
-    workers_per_gpu=2,
+    workers_per_gpu=8,
     train=dict(pipeline=train_pipeline),
     val=dict(pipeline=test_pipeline),
     test=dict(pipeline=test_pipeline))
